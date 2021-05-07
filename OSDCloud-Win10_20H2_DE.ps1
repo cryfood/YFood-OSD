c55d2a392d3eb7b2b43f7c68133bd14cba278098
cryfood <82637877+cryfood@users.noreply.github.com>
2021-05-07 15:19:36 +0200

    Update OSDCloud-Win10_20H2_DE.ps1

--- OSDCloud-Win10_20H2_DE.ps1
+++ OSDCloud-Win10_20H2_DE.ps1
@@ -26,8 +26,7 @@ Import-Module OSD -Force
 
 Write-Host -ForegroundColor Green "Start OSDCloud"
 
-#Start-OSDCloud -OSLanguage de-de -OSBuild 20H2 -OSEdition Pro -ZTI
-Start-OSDCloud -FindImageFile -ZTI
+Start-OSDCloud -OSLanguage de-de -OSBuild 20H2 -OSEdition Pro -ZTI
 
 #Restart from WinPE
 
